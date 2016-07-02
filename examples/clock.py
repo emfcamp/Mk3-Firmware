9016a99ea98dc0f058e984b356ad77089300cafd
Marek <marekventur@gmail.com>
2016-07-02 01:04:19 +0100

    Fix clock demo

--- examples/clock.py
+++ examples/clock.py
@@ -1,47 +1,48 @@
 import pyb
 import math
+import ugfx
 
 # Example of how a simple animation can be done
 # ToDo: This is quite flickery. It would work a lot better with
 #       Pixmaps, but I couldn't get them to work :(
 
-lcd = pyb.UGFX()
+ugfx.init()
 
 sec = 0;
 
-def draw_hand(lcd, cx, cy, angle, length, thickness, color):
+def draw_hand(cx, cy, angle, length, thickness, color):
     x = int(math.cos(angle) * length + cx);
     y = int(math.sin(angle) * length + cy);
-    lcd.thickline(cx, cy, x, y, color, thickness, 1)
+    ugfx.thickline(cx, cy, x, y, color, thickness, 1)
 
 while True:
-    lcd.area(0, 0, lcd.get_width(), lcd.get_height(), 0)
+    ugfx.area(0, 0, ugfx.width(), ugfx.height(), 0)
 
     # Center
-    cx = int(lcd.get_width() / 2);
-    cy = int(lcd.get_height() / 2);
+    cx = int(ugfx.width() / 2);
+    cy = int(ugfx.height() / 2);
 
     # Clock face
-    lcd.circle(cx, cy, 70, pyb.UGFX.WHITE)
+    ugfx.circle(cx, cy, 70, ugfx.WHITE)
     for i in range(0, 12):
         a = math.pi * 2 * i / 12
         x1 = int(math.cos(a) * 55 + cx);
         y1 = int(math.sin(a) * 55 + cy);
         x2 = int(math.cos(a) * 60 + cx);
         y2 = int(math.sin(a) * 60 + cy);
-        lcd.line(x1, y1, x2, y2, pyb.UGFX.WHITE)
+        ugfx.line(x1, y1, x2, y2, ugfx.WHITE)
 
     # Hand: hours
     angel_hour = math.pi * 2 * sec / 60 / 60 / 12 - math.pi / 2
-    draw_hand(lcd, cx, cy, angel_hour, 35, 4, pyb.UGFX.YELLOW)
+    draw_hand(cx, cy, angel_hour, 35, 4, ugfx.YELLOW)
 
     # Hand: minutes
     angel_min = math.pi * 2 * sec / 60 / 60 - math.pi / 2
-    draw_hand(lcd, cx, cy, angel_min, 40, 2, pyb.UGFX.WHITE)
+    draw_hand(cx, cy, angel_min, 40, 2, ugfx.WHITE)
 
     # Hand: seconds
     angel_seconds = math.pi * 2 * sec / 60 - math.pi / 2
-    draw_hand(lcd, cx, cy, angel_seconds, 50, 1, pyb.UGFX.RED)
+    draw_hand(cx, cy, angel_seconds, 50, 1, ugfx.RED)
 
     # Wait
     pyb.delay(10)
