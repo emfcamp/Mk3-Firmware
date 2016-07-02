6a16ad979bb0f8e7c1c0ec0381b275f63b66d47e
Marek <marekventur@gmail.com>
2016-07-02 01:01:46 +0100

    Fix name.py example

--- examples/name.py
+++ examples/name.py
@@ -1,10 +1,11 @@
 import ugfx
 import pyb
 
-def: display_name():
-	ugfx.area(0,0,ugfx.get_width(),ugfx.get_height(),0xFFFF)
+def display_name():
+	ugfx.area(0,0,ugfx.width(),ugfx.height(),0xFFFF)
 	ugfx.set_default_font("D*")
 	ugfx.text(40,120,"MATT",ugfx.YELLOW)
 	ugfx.circle(160,200,40,ugfx.GREEN)
-	
+
+ugfx.init()
 display_name()
