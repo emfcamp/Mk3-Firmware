899ea66bb2514275e3c07f3428ab3b3563f74a25
Marek <marekventur@gmail.com>
2016-07-04 23:52:58 +0100

    Update lib/http-client.py with working download function

--- lib/http_client.py
+++ lib/http_client.py
@@ -1,6 +1,6 @@
-##################
-# http_client.py #
-##################
+### Author: EMF Badge team
+### Description: A basic HTTP library, based on https://github.com/balloob/micropython-http-client
+### License: MIT
 
 import usocket
 import ujson
@@ -43,176 +43,139 @@ class Response(object):
         if 500 <= self.status_code < 600:
             raise OSError('Server error: %s' % self.status_code)
 
-# Returns http status line
-# Supports \n, \r and \r\n
-def read_http_status(buffer):
-    i = buffer.find(b"\n")
-    j = buffer.find(b"\r")
-    if i == -1 and j == -1:
-        raise OSError('Invalid HTTP status header')
-    if i == -1:
-        j = i
+def open_http_socket(method, url, json=None, timeout=None, headers=None):
+    urlparts = url.split('/', 3)
+    proto = urlparts[0]
+    host = urlparts[2]
+    urlpath = '' if len(urlparts) < 4 else urlparts[3]
+
+    if proto == 'http:':
+        port = 80
+    elif proto == 'https:':
+        port = 443
     else:
-        if j != -1:
-            i = min(i, j)
+        raise OSError('Unsupported protocol: %s' % proto[:-1])
 
-    line = str(buffer[:i], 'utf-8')
+    if ':' in host:
+        host, port = host.split(':')
+        port = int(port)
 
-    return line
+    if json is not None:
+        content = ujson.dumps(json)
+        content_type = CONTENT_TYPE_JSON
+    else:
+        content = None
 
-# Returns index of first http content byte, skipping all headers
-def get_content_index(buffer):
-    indexes = [
-        buffer.find(b"\n\n"),
-        buffer.find(b"\r\r"),
-        buffer.find(b"\r\n\r\n")
-    ]
+    ai = usocket.getaddrinfo(host, port)
+    addr = ai[0][4]
 
-    indexes = [ index for index in indexes if index > -1 ] # remove -1s
+    sock = usocket.socket()
 
-    if len(indexes) == 0:
-        raise OSError('Invalid HTTP response, no double newline found')
+    if timeout is not None:
+        assert SUPPORT_TIMEOUT, 'Socket does not support timeout'
+        sock.settimeout(timeout)
 
-    i = min(indexes) + 2
-    if buffer[i:i+2] == b"\r\n":
-        i += 2
-    return i
+    sock.connect(addr)
+
+    if proto == 'https:':
+        assert SUPPORT_SSL, 'HTTPS not supported: could not find ussl'
+        sock = ussl.wrap_socket(sock)
+
+    sock.send('%s /%s HTTP/1.0\r\nHost: %s\r\n' % (method, urlpath, host))
+
+    if headers is not None:
+        for header in headers.items():
+            sock.send('%s: %s\r\n' % header)
+
+    if content is not None:
+        sock.send('content-length: %s\r\n' % len(content))
+        sock.send('content-type: %s\r\n' % content_type)
+        sock.send('\r\n')
+        sock.send(content)
+    else:
+        sock.send('\r\n')
+
+    return sock
 
 # Adapted from upip
 def request(method, url, json=None, timeout=None, headers=None):
-    urlparts = url.split('/', 3)
-    proto = urlparts[0]
-    host = urlparts[2]
-    urlpath = '' if len(urlparts) < 4 else urlparts[3]
-
-    if proto == 'http:':
-        port = 80
-    elif proto == 'https:':
-        port = 443
-    else:
-        raise OSError('Unsupported protocol: %s' % proto[:-1])
-
-    if ':' in host:
-        host, port = host.split(':')
-        port = int(port)
-
-    if json is not None:
-        content = ujson.dumps(json)
-        content_type = CONTENT_TYPE_JSON
-    else:
-        content = None
-
-    ai = usocket.getaddrinfo(host, port)
-    addr = ai[0][4]
-
-    sock = usocket.socket()
-
-    if timeout is not None:
-        assert SUPPORT_TIMEOUT, 'Socket does not support timeout'
-        sock.settimeout(timeout)
-
-    sock.connect(addr)
-
-    if proto == 'https:':
-        assert SUPPORT_SSL, 'HTTPS not supported: could not find ussl'
-        sock = ussl.wrap_socket(sock)
-
-    sock.send('%s /%s HTTP/1.0\r\nHost: %s\r\n' % (method, urlpath, host))
-
-    if headers is not None:
-        for header in headers.items():
-            sock.send('%s: %s\r\n' % header)
-
-    if content is not None:
-        sock.send('content-length: %s\r\n' % len(content))
-        sock.send('content-type: %s\r\n' % content_type)
-        sock.send('\r\n')
-        sock.send(content)
-    else:
-        sock.send('\r\n')
-
-    # Consume the whole response
-    buffer = sock.recv(1024)
-    while True:
-        received = sock.recv(1024)
-        if len(received) == 0:
-            break
-        buffer += received
-
-    # Read status line
-    protover, status, msg = read_http_status(buffer).split(None, 2)
-
-    content_index = get_content_index(buffer)
-
-    # Return response object
-    return Response(int(status), buffer[content_index:])
-
-def download(method, url, target, json=None, timeout=None, headers=None):
-    urlparts = url.split('/', 3)
-    proto = urlparts[0]
-    host = urlparts[2]
-    urlpath = '' if len(urlparts) < 4 else urlparts[3]
-
-    if proto == 'http:':
-        port = 80
-    elif proto == 'https:':
-        port = 443
-    else:
-        raise OSError('Unsupported protocol: %s' % proto[:-1])
-
-    if ':' in host:
-        host, port = host.split(':')
-        port = int(port)
-
-    if json is not None:
-        content = ujson.dumps(json)
-        content_type = CONTENT_TYPE_JSON
-    else:
-        content = None
-
-    ai = usocket.getaddrinfo(host, port)
-    addr = ai[0][4]
-
-    sock = usocket.socket()
-
-    if timeout is not None:
-        assert SUPPORT_TIMEOUT, 'Socket does not support timeout'
-        sock.settimeout(timeout)
-
-    sock.connect(addr)
-
-    if proto == 'https:':
-        assert SUPPORT_SSL, 'HTTPS not supported: could not find ussl'
-        sock = ussl.wrap_socket(sock)
-
-    sock.send('%s /%s HTTP/1.0\r\nHost: %s\r\n' % (method, urlpath, host))
-
-    if headers is not None:
-        for header in headers.items():
-            sock.send('%s: %s\r\n' % header)
-
-    if content is not None:
-        sock.send('content-length: %s\r\n' % len(content))
-        sock.send('content-type: %s\r\n' % content_type)
-        sock.send('\r\n')
-        sock.send(content)
-    else:
-        sock.send('\r\n')
-
-    # write to disk
-    # todo: skip headers
-    with open(target, 'wb') as output:
+    sock = open_http_socket(method, url, json, timeout, headers)
+    try:
+        # Consume the whole response
+        buffer = sock.recv(1024)
         while True:
+            pyb.delay(50)
             received = sock.recv(1024)
             if len(received) == 0:
                 break
-            output.write(received)
+            buffer += received
 
+        # Read status line
+        protover, status, msg = read_http_status(buffer).split(None, 2)
 
+        content_index = get_content_index(buffer)
+
+        # Return response object
+        return Response(int(status), buffer[content_index:])
+    finally:
+        sock.close();
+
+def download(method, url, target, json=None, timeout=None, headers=None):
+    sock = open_http_socket(method, url, json, timeout, headers)
+
+    try:
+        with open(target, 'wb') as f:
+            state = 1
+            hbuf = b"";
+            remaining = None;
+            while True:
+                buf = sock.recv(1024)
+                print(len(buf))
+                if state == 1: # Status
+                    nl = buf.find(b"\n")
+                    if nl > -1:
+                        hbuf += buf[:nl - 1]
+                        status = hbuf.split(b' ')[1]
+                        if status != b"200":
+                            raise Exception("Invalid status " + str(status))
+
+                        state = 2
+                        hbuf = b"";
+                        buf = buf[nl + 1:]
+                    else:
+                        hbuf += buf
+
+                if state == 2: # Headers
+                    hbuf += buf
+                    nl = hbuf.find(b"\n")
+                    while nl > -1:
+                        if nl < 2:
+                            if remaining == None:
+                                raise Exception("No Content-Length")
+                            buf = hbuf[2:]
+                            hbuf = None
+                            state = 3
+                            break
+
+                        header = hbuf[:nl - 1].decode("utf8").split(':', 3)
+                        if header[0] == "Content-Length":
+                            remaining = int(header[1].strip())
+
+                        hbuf = hbuf[nl + 1:]
+                        nl = hbuf.find(b"\n")
+
+                if state == 3: # Content
+                    f.write(buf)
+                    remaining -= len(buf)
+                    if remaining < 1:
+                        break
+
+                pyb.delay(50)
+    finally:
+        sock.close()
 
 def get(url, **kwargs):
     return request('GET', url, **kwargs)
-
 
 def post(url, **kwargs):
     return request('POST', url, **kwargs)
