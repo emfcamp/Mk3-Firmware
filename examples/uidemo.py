0466927109cf8ed11cebc48785a9a4946d26faae
Marek <marekventur@gmail.com>
2016-07-06 21:04:57 +0100

    Update uidemo

--- examples/uidemo.py
+++ examples/uidemo.py
@@ -13,7 +13,7 @@ ugfx.text(40, 0, "EMF BADGE 2016", ugfx.PURPLE)
 
 ugfx.set_default_font("C*")
 
-options = ugfx.List(10,50,160,200)
+options = ugfx.List(0,0,160,150)
 btn_ok = ugfx.Button(200,50,70,30,"A: Run")
 btn_menu = ugfx.Button(200,90,70,30,"M: Menu")
 
@@ -21,8 +21,6 @@ files = os.listdir()
 
 for f in files:
 	options.add_item(f)
-	
-options.attach_input(ugfx.JOY_UP,1)
-options.attach_input(ugfx.JOY_DOWN,0)
+
 btn_menu.attach_input(ugfx.BTN_MENU)
 btn_ok.attach_input(ugfx.BTN_A)
