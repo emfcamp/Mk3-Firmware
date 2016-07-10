20d97f2978a6c4d310e13283fe82c5fb253fa4e6
Marek <marekventur@gmail.com>
2016-07-10 20:27:25 +0100

    Update home app to new libraries

--- apps/home/main.py
+++ apps/home/main.py
@@ -1,11 +1,13 @@
 import ugfx
 import pyb
+from database import *
+import buttons
 
 def display_name():
 	ugfx.area(0,0,ugfx.width(),ugfx.height(),0xFFFF)
 	ugfx.set_default_font("D*")
 	ugfx.text(40,90,"My name is...",ugfx.BLUE)
-	ugfx.text(40,120,"MATT",ugfx.BLUE)
+	ugfx.text(40,120,database_get("name", "<not set yet>"),ugfx.BLUE)
 	ugfx.circle(140,200,40,ugfx.GREEN)
 	ugfx.circle(160,200,40,ugfx.GREEN)
 	ugfx.circle(180,200,40,ugfx.GREEN)
@@ -14,20 +16,12 @@ def button_press():
 	import apps.home.quick_launch
 	apps.home.quick_launch.main()
 
-
-	display_name()
-	pyb.delay(1500)
-
-def main():
-	display_name()
-
-	tgl_menu = pyb.Pin("BTN_MENU", pyb.Pin.IN)
-	tgl_menu.init(pyb.Pin.IN, pyb.Pin.PULL_UP)
-
-	while True:
-		pyb.wfi()
-		if tgl_menu.value() == 0:
-			button_press()
-
 ugfx.init()
-main()
+display_name()
+
+buttons.init()
+
+while True:
+	pyb.wfi()
+	if buttons.is_triggered("BTN_MENU"):
+		button_press()
