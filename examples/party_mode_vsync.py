daa25d9d1d7d9bdeb48bcba9441090c3424ae800
Matt Brejza <mbrejza@gmail.com>
2016-07-12 00:00:02 +0100

    now works sideways

--- examples/party_mode_vsync.py
+++ examples/party_mode_vsync.py
@@ -7,41 +7,43 @@ tgl_menu = pyb.Pin("BTN_MENU", pyb.Pin.IN)
 tgl_menu.init(pyb.Pin.IN, pyb.Pin.PULL_UP)
 ugfx.enable_tear()
 tear = pyb.Pin("TEAR", pyb.Pin.IN)
+wi = ugfx.width()
+hi = ugfx.height()
 while(keepgoing):
 	
 	while(tear.value() == 0):
 		2+2 
 	while(tear.value()):
 		2+2 
-	ugfx.area(0,0,320,240,ugfx.RED)		
+	ugfx.area(0,0,wi,hi,ugfx.RED)
 	pyb.delay(60)
 	
 	while(tear.value() == 0):
 		2+2 
 	while(tear.value()):
 		2+2 
-	ugfx.area(0,0,320,240,ugfx.GREEN)		
+	ugfx.area(0,0,wi,hi,ugfx.GREEN)
 	pyb.delay(60)
 	
 	while(tear.value() == 0):
 		2+2 
 	while(tear.value()):
 		2+2 
-	ugfx.area(0,0,320,240,ugfx.YELLOW)
+	ugfx.area(0,0,wi,hi,ugfx.YELLOW)
 	pyb.delay(60)
 	
 	while(tear.value() == 0):
 		2+2 
 	while(tear.value()):
 		2+2 
-	ugfx.area(0,0,320,240,ugfx.WHITE)
+	ugfx.area(0,0,wi,hi,ugfx.WHITE)
 	pyb.delay(60)
 	
 	while(tear.value() == 0):
 		2+2 
 	while(tear.value()):
 		2+2 
-	ugfx.area(0,0,320,240,ugfx.BLUE)
+	ugfx.area(0,0,wi,hi,ugfx.BLUE)
 	pyb.delay(60)
 		
 	if tgl_menu.value() == 0:
