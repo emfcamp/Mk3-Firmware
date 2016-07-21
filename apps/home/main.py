0e7c30f4ab6672db1531459e51fa3abd2195d018
Matt Brejza <mbrejza@gmail.com>
2016-07-22 00:34:38 +0100

    adding battery graphic

--- apps/home/main.py
+++ apps/home/main.py
@@ -13,6 +13,16 @@ def display_name():
 	ugfx.circle(140,200,40,ugfx.GREEN)
 	ugfx.circle(160,200,40,ugfx.GREEN)
 	ugfx.circle(180,200,40,ugfx.GREEN)
+	
+def draw_battery(x,y,back_colour,percent):
+	ugfx.set_default_font("c*")	
+	ugfx.text(x+35,y,str(int(percent)),back_colour^0xFFFF)	
+	y += 2
+	ugfx.area(x,y,30,11,back_colour^0xFFFF)
+	ugfx.area(x+30,y+3,3,5,back_colour^0xFFFF)	
+	ugfx.area(x+2,y+2,26,7,back_colour)
+	ugfx.area(x+2,y+2,int(percent*26/100),7,back_colour^0xFFFF)
+	
 
 while True:
 	ugfx.init()
@@ -21,6 +31,9 @@ while True:
 	buttons.init()
 	
 	gc.collect()
+	
+	draw_battery(3,3,0xFFFF,49)
+
 
 	while not buttons.is_triggered("BTN_MENU"):
 		pyb.wfi()
