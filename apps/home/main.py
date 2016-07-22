ee4bfc098aaf9208e6c6f040462d5d4d01b549b4
Matt Brejza <mbrejza@gmail.com>
2016-07-22 12:17:20 +0100

    starting screen brightness

--- apps/home/main.py
+++ apps/home/main.py
@@ -22,7 +22,24 @@ def draw_battery(x,y,back_colour,percent):
 	ugfx.area(x+30,y+3,3,5,back_colour^0xFFFF)	
 	ugfx.area(x+2,y+2,26,7,back_colour)
 	ugfx.area(x+2,y+2,int(percent*26/100),7,back_colour^0xFFFF)
+
+tick = 0;
 	
+def tick_inc():
+	global tick
+	tick += 1
+	
+def backlight_adjust():
+	l = pyb.ADC(16).read()
+	if (l > 90):
+		ugfx.backlight(100)
+	elif (l > 20):
+		ugfx.backlight(70)
+	else:
+		ugfx.backlight(30)
+
+
+
 
 while True:
 	ugfx.init()
@@ -33,10 +50,25 @@ while True:
 	gc.collect()
 	
 	draw_battery(3,3,0xFFFF,49)
-
-
-	while not buttons.is_triggered("BTN_MENU"):
+	
+	timerb = pyb.Timer(16)
+	timerb.init(freq=1)
+	timerb.callback(tick_inc())
+	
+	while True:
 		pyb.wfi()
+		
+		if tick > 0:
+			tick = 0
+			backlight_adjust()
+			draw_battery(3,3,0xFFFF,49)
+
+		if buttons.is_triggered("BTN_MENU"):
+			break
+		
+		
+	timerb.deinit()
+	ugfx.backlight(100)
 
 	## ToDo: Maybe boot should always chdir to the app folder?
 	execfile("apps/home/quick_launch.py")
