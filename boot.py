580c3a73a20eec59b61cabe526320fda02e59d96
Marek <marekventur@gmail.com>
2016-07-23 12:36:58 +0100

    Golfing some bytes of boot.py

--- boot.py
+++ boot.py
@@ -1,21 +1,13 @@
-import pyb
-import json
-import os
-import micropython
+import pyb, json, os, micropython
 
 micropython.alloc_emergency_exception_buf(100)
 
-entrypoint = "bootstrap.py"
+m = "bootstrap.py"
 if "apps" in os.listdir():
-    apps = os.listdir("apps")
-    if "home" in apps:
-        entrypoint = "apps/home/main.py"
-    elif "updater" in apps:
-        entrypoint = "apps/updater/main.py"
+	apps = os.listdir("apps")
+	if "home" in apps:
+		m = "apps/home/main.py"
+	elif "updater" in apps:
+		m = "apps/updater/main.py"
 
-pyb.main(entrypoint)
-
-
-
-
-
+pyb.main(m)
