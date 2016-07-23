ba56a7730d28977189340643abc041fab29beda9
matt brejza <mfb2g09>
2016-07-23 13:19:34 +0100

    battery voltage updates

--- apps/home/main.py
+++ apps/home/main.py
@@ -4,6 +4,7 @@ import os
 from database import *
 import buttons
 import gc
+import stm
 
 def display_name():
 	ugfx.area(0,0,ugfx.width(),ugfx.height(),0xFFFF)
@@ -16,16 +17,17 @@ def display_name():
 	
 def draw_battery(x,y,back_colour,percent):
 	ugfx.set_default_font("c*")	
+	ugfx.area(x+35,y,40,25,back_colour)
 	ugfx.text(x+35,y,str(int(percent)),back_colour^0xFFFF)	
 	y += 2
 	ugfx.area(x,y,30,11,back_colour^0xFFFF)
 	ugfx.area(x+30,y+3,3,5,back_colour^0xFFFF)	
 	ugfx.area(x+2,y+2,26,7,back_colour)
-	ugfx.area(x+2,y+2,int(percent*26/100),7,back_colour^0xFFFF)
+	ugfx.area(x+2,y+2,int(min(percent,100)*26/100),7,back_colour^0xFFFF)
 
-tick = 0;
+tick = 0
 	
-def tick_inc():
+def tick_inc(t):
 	global tick
 	tick += 1
 	
@@ -38,8 +40,14 @@ def backlight_adjust():
 	else:
 		ugfx.backlight(30)
 
+def get_battery_voltage():
+#	ref = pyb.ADC(0).read()
+	vin = pyb.ADC(4).read()
+	return 2 * vin / 4096 * 3.3
 
-
+timerb = pyb.Timer(3)
+timerb.init(freq=1)
+timerb.callback(tick_inc)
 
 while True:
 	ugfx.init()
@@ -49,11 +57,7 @@ while True:
 	
 	gc.collect()
 	
-	draw_battery(3,3,0xFFFF,49)
-	
-	timerb = pyb.Timer(16)
-	timerb.init(freq=1)
-	timerb.callback(tick_inc())
+	draw_battery(3,3,0xFFFF,49)	
 	
 	while True:
 		pyb.wfi()
@@ -61,11 +65,13 @@ while True:
 		if tick > 0:
 			tick = 0
 			backlight_adjust()
-			draw_battery(3,3,0xFFFF,49)
+			
+			v = get_battery_voltage();
+			draw_battery(3,3,0xFFFF,int(v/4.2*100))
 
 		if buttons.is_triggered("BTN_MENU"):
 			break
-		
+
 		
 	timerb.deinit()
 	ugfx.backlight(100)
