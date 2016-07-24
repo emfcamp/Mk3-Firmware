f42f949b4fdb16c3a8fd7d858456e6aebf71e925
Marek <marekventur@gmail.com>
2016-07-24 13:28:46 +0100

    Update bootstrap code

--- boot.py
+++ boot.py
@@ -1,3 +1,9 @@
+# boot.py -- run on boot-up
+# can run arbitrary Python, but best to keep it minimal
+# this is a special version for EMF
+#pyb.usb_mode('CDC+MSC') # act as a serial and a storage device
+#pyb.usb_mode('CDC+HID') # act as a serial device and a mouse
+
 import pyb, json, os, micropython
 
 micropython.alloc_emergency_exception_buf(100)
@@ -7,7 +13,7 @@ if "apps" in os.listdir():
 	apps = os.listdir("apps")
 	if "home" in apps:
 		m = "apps/home/main.py"
-	elif "updater" in apps:
+	elif "widget_store" in apps:
 		m = "apps/widget_store/main.py"
 
 pyb.main(m)
--- bootstrap.py
+++ bootstrap.py
@@ -3,60 +3,60 @@
 import network, pyb, usocket, machine, os, json, ugfx
 
 def download(path, target):
-    host = "api.badge.emfcamp.org"
-    ai = usocket.getaddrinfo(host, 80) # ToDo: Change to 443 once we have https
-    s = usocket.socket()
-    s.connect(ai[0][4]) # .wrap_socket(s) # enable HTTPS
-    s.send('GET /%s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n' % (path, host))
+	host = "api.badge.emfcamp.org"
+	ai = usocket.getaddrinfo(host, 80) # ToDo: Change to 443 once we have https
+	s = usocket.socket()
+	s.connect(ai[0][4]) # .wrap_socket(s) # enable HTTPS
+	s.send('GET /%s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n' % (path, host))
 
-    with open(target, 'wb') as f:
-        state = 1
-        hbuf = b"";
-        remaining = None;
-        while True:
-            buf = s.recv(1024)
-            if state == 1: # Status
-                nl = buf.find(b"\n")
-                if nl > -1:
-                    hbuf += buf[:nl - 1]
-                    status = hbuf.split(b' ')[1]
-                    if status != b"200":
-                        raise Exception("Invalid status " + str(status))
+	with open(target, 'wb') as f:
+		state = 1
+		hbuf = b"";
+		remaining = None;
+		while True:
+			buf = s.recv(1024)
+			if state == 1: # Status
+				nl = buf.find(b"\n")
+				if nl > -1:
+					hbuf += buf[:nl - 1]
+					status = hbuf.split(b' ')[1]
+					if status != b"200":
+						raise Exception("Invalid status " + str(status))
 
-                    state = 2
-                    hbuf = b"";
-                    buf = buf[nl + 1:]
-                else:
-                    hbuf += buf
+					state = 2
+					hbuf = b"";
+					buf = buf[nl + 1:]
+				else:
+					hbuf += buf
 
-            if state == 2: # Headers
-                hbuf += buf
-                nl = hbuf.find(b"\n")
-                while nl > -1:
-                    if nl < 2:
-                        if remaining == None:
-                            raise Exception("No Content-Length")
-                        buf = hbuf[2:]
-                        hbuf = None
-                        state = 3
-                        break
+			if state == 2: # Headers
+				hbuf += buf
+				nl = hbuf.find(b"\n")
+				while nl > -1:
+					if nl < 2:
+						if remaining == None:
+							raise Exception("No Content-Length")
+						buf = hbuf[2:]
+						hbuf = None
+						state = 3
+						break
 
-                    header = hbuf[:nl - 1].decode("utf8").split(':', 3)
-                    if header[0] == "Content-Length":
-                        remaining = int(header[1].strip())
+					header = hbuf[:nl - 1].decode("utf8").split(':', 3)
+					if header[0] == "Content-Length":
+						remaining = int(header[1].strip())
 
-                    hbuf = hbuf[nl + 1:]
-                    nl = hbuf.find(b"\n")
+					hbuf = hbuf[nl + 1:]
+					nl = hbuf.find(b"\n")
 
-            if state == 3: # Content
-                f.write(buf)
-                remaining -= len(buf)
-                if remaining < 1:
-                    break
+			if state == 3: # Content
+				f.write(buf)
+				remaining -= len(buf)
+				if remaining < 1:
+					break
 
-            pyb.delay(50)
+			pyb.delay(50)
 
-    s.close()
+	s.close()
 
 ugfx.init()
 ugfx.clear(ugfx.BLACK)
@@ -66,29 +66,29 @@ ugfx.text(0, 50, "pressing the reset button at the back", ugfx.WHITE)
 ugfx.text(0, 100, "Please wait...", ugfx.WHITE)
 
 def message(lines):
-    y = 150
-    ugfx.area(0, y, ugfx.width(), ugfx.height() - y, ugfx.BLACK)
-    for line in lines:
-        ugfx.text(0, y, line, ugfx.WHITE)
-        y += 20
+	y = 150
+	ugfx.area(0, y, ugfx.width(), ugfx.height() - y, ugfx.BLACK)
+	for line in lines:
+		ugfx.text(0, y, line, ugfx.WHITE)
+		y += 20
 
 for d in ["apps", "apps/widget_store", "lib"]:
-    try:
-        os.mkdir(d)
-    except OSError as e:
-        print(e)
+	try:
+		os.mkdir(d)
+	except OSError as e:
+		print(e)
 
 w = {}
 try:
-    if "wifi.json" in os.listdir():
-        with open("wifi.json") as f:
-            w = json.loads(f.read())
+	if "wifi.json" in os.listdir():
+		with open("wifi.json") as f:
+			w = json.loads(f.read())
 except ValueError as e:
-    print(e)
+	print(e)
 
 if not ("ssid" in w and "pw" in w):
-    message(["Couldn't find a valid wifi.json :(", "More information:", "badge.emfcamp.org/TiLDA_MK3/wifi"])
-    while True: pyb.wfi()
+	message(["Couldn't find a valid wifi.json :(", "More information:", "badge.emfcamp.org/TiLDA_MK3/wifi"])
+	while True: pyb.wfi()
 
 wifi_ssid = w["ssid"]
 wifi_pw = w["pw"]
@@ -97,18 +97,18 @@ message(["Connecting to '%s'" % (wifi_ssid), "Update wifi.json if this is incorr
 n = network.CC3100()
 n.connect(wifi_ssid, wifi_pw)
 while not n.is_connected():
-    n.update()
-    pyb.delay(100)
+	n.update()
+	pyb.delay(100)
 
 try:
-    libs = ["wifi", "buttons", "http_client", "filesystem", "dialogs", "database"]
-    for i, lib in enumerate(libs):
-        message(["Downloading library: %s (%d/%d)" % (lib, i + 1, len(libs))])
-        download("/firmware/master/lib/%s.py" % (lib), "lib/%s.py" % (lib))
+	libs = ["wifi", "buttons", "http_client", "filesystem", "dialogs", "database"]
+	for i, lib in enumerate(libs):
+		message(["Downloading library: %s (%d/%d)" % (lib, i + 1, len(libs))])
+		download("/firmware/master/lib/%s.py" % (lib), "lib/%s.py" % (lib))
 
-    message(["Downloading widget store"])
-    download("/firmware/master/apps/widget_store/main.py", "apps/widget_store/main.py")
+	message(["Downloading widget store"])
+	download("/firmware/master/apps/widget_store/main.py", "apps/widget_store/main.py")
 
 finally:
-    os.sync()
+	os.sync()
 machine.reset()
