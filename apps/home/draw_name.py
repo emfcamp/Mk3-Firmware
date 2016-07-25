c5f38be1b5ad159b6d28031570ac58786b6a7b9d
Matt Brejza <mbrejza@gmail.com>
2016-07-25 23:27:40 +0100

    updating name display

--- apps/home/draw_name.py
+++ apps/home/draw_name.py
@@ -7,18 +7,23 @@ import buttons
 import gc
 import stm
 
+obj = []
 
 def draw(window):
 	#ToDo: change to use window._____ rather than ugfx.____
+
+	ugfx.set_default_font(ugfx.FONT_NAME)
+	l=ugfx.Label(5,20,310,window.height()-20,database_get("display-name", "<not actually set yet>"),parent=window)
+	obj.append(l)
+	ugfx.set_default_font(ugfx.FONT_MEDIUM_BOLD)
+	obj.append(ugfx.Label(5,0,310,20,"My name is...",parent=window))
+	#ugfx.text(40,80,database_get("display-name", "<not set yet>"),ugfx.BLUE)
+	#ugfx.circle(140,150,40,ugfx.GREEN)
+	#ugfx.circle(160,150,40,ugfx.GREEN)
+	#ugfx.circle(180,150,40,ugfx.GREEN)
 	window.show()
-	ugfx.set_default_font("D*")
-	window.text(40,40,"My name is...",ugfx.BLUE)
-	window.text(40,80,database_get("display-name", "<not set yet>"),ugfx.BLUE)
-	window.circle(140,150,40,ugfx.GREEN)
-	window.circle(160,150,40,ugfx.GREEN)
-	window.circle(180,150,40,ugfx.GREEN)
-	
 	
 def draw_destroy(obj_name):
 #there may be some .destroy() functions that could be wanted to be called
-	pass
+	for o in obj:
+		o.destroy()
