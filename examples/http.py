38991fae0f9a5d4d910c83ca41c7599b0d53c49d
Matt Brejza <mbrejza@gmail.com>
2016-07-31 15:16:56 +0100

    adding error checking on wifi example

--- examples/http.py
+++ examples/http.py
@@ -3,6 +3,9 @@ from http_client import *
 
 wifi.connect()
 
-ip = get('http://httpbin.org/ip').raise_for_status().json()["origin"]
-print("My public IP is %s" %(ip))
-
+try:
+	if wifi.nic().is_connected():
+		ip = get('http://httpbin.org/ip').raise_for_status().json()["origin"]
+		print("My public IP is %s" %(ip))
+except OSError as e:
+		print("Query failed " + str(e))
