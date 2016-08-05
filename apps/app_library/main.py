61cf040740f5e21df3c502235ea7fccf10786c84
matt brejza <mfb2g09>
2016-08-05 17:01:45 +0100

    added default apps

--- apps/app_library/main.py
+++ apps/app_library/main.py
@@ -174,6 +174,6 @@ def remove():
 if App("home").loadable:
     main_menu()
 else:
-    for app_name in ["home", "sponsors", "changename", "microsoft~barms_logger", "snake", "nexmo~messages"]:
+    for app_name in ["home", "sponsors", "changename", "microsoft~barms-logger", "snake", "nexmo~messages"]:
         install(App(app_name))
 
