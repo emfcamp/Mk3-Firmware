8c0979774f5e0397d43bbbd366e7039231520fb5
mattbrejza <mbrejza@gmail.com>
2016-08-09 20:41:14 +0100

    Revert "Add ability to run script as main.py"

--- boot.py
+++ boot.py
@@ -12,18 +12,9 @@ m = "bootstrap.py"
 if "main.py" in os.listdir():
     m = "main.py"
 elif "apps" in os.listdir():
-    apps = os.listdir("apps")
-    if ("home" in apps) and ("main.py" in os.listdir("apps/home")):
-        m = "apps/home/main.py"
-    elif ("app_library" in apps) and ("main.py" in os.listdir("apps/app_library")):
-        m = "apps/app_library/main.py"
-if 'main.json' in os.listdir():
-    try:
-        with open('main.json', 'r') as f:
-            main_dict = json.loads(f.read())
-            m = main_dict['main']
-            if not main_dict.get('perm', False):
-                os.remove('main.json')
-    except:
-        pass
+	apps = os.listdir("apps")
+	if ("home" in apps) and ("main.py" in os.listdir("apps/home")):
+		m = "apps/home/main.py"
+	elif ("app_library" in apps) and ("main.py" in os.listdir("apps/app_library")):
+		m = "apps/app_library/main.py"
 pyb.main(m)
