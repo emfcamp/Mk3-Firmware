3fddd173b5901270c7c41ac7319bd803bbe74297
Matt <mfb2g09@soton.ac.uk>
2016-08-11 14:14:59 +0100

    now defaults to restarting all apps before running

--- apps/home/file_loader.py
+++ apps/home/file_loader.py
@@ -154,8 +154,8 @@ if app_to_load:
 	buttons.enable_menu_reset()
 	import run_app
 	rbr = app_to_load.get_attribute("reboot-before-run")
-	if type(rbr) == str and rbr.lower() == "true": 
-		run_app.reset_and_run(app_to_load.main_path[:-3])
-	run_app.run_app(app_to_load.main_path[:-3])
+	if type(rbr) == str and rbr.lower() == "false": 
+      run_app.run_app(app_to_load.main_path[:-3])
+	run_app.reset_and_run(app_to_load.main_path[:-3])
 	
 	
