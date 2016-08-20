efc64f636b8bf9538b8c74a470e95771d9b6dbee
Thibault <thibault.ml@bromium.com>
2016-08-20 01:46:55 +0100

    [#61] Use non-blocking receive to prevent home from hanging
    This change makes sure we do not wait too long to receive (`read()`) data from the NTP server in case it cannot be reached, or is too slow to respond.
    However, because we're using UDP to communicate with NTP, there's no need to wait for the `sendto()` to succeed.

--- lib/ntp.py
+++ lib/ntp.py
@@ -9,16 +9,30 @@ import socket
 # (date(2000, 1, 1) - date(1900, 1, 1)).days * 24*60*60
 NTP_DELTA = 3155673600
 NTP_HOST = "pool.ntp.org"
+NTP_PORT = 123
 
 
 def get_NTP_time():
 	NTP_QUERY = bytearray(48)
 	NTP_QUERY[0] = 0x1b
-	addr = socket.getaddrinfo(NTP_HOST, 123)[0][-1]
+	addr = socket.getaddrinfo(NTP_HOST, NTP_PORT)[0][-1]
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	res = s.sendto(NTP_QUERY, addr)
-	msg = s.recv(48)
-	s.close()
+
+	# Setting timeout for receiving data. Because we're using UDP,
+	# there's no need for a timeout on send.
+	s.settimeout(2)
+	msg = None
+	try:
+		msg = s.recv(48)
+	except OSError:
+		pass
+	finally:
+		s.close()
+
+	if msg is None:
+		return None
+
 	import struct
 	val = struct.unpack("!I", msg[40:44])[0]
 	return val - NTP_DELTA
@@ -29,11 +43,15 @@ def set_NTP_time():
 	from pyb import RTC
 	print("Setting time from NTP")
 
+	t = get_NTP_time()
+	if t is None:
+		print("Could not set time from NTP")
+		return
+
 	tz = 0
 	with database.Database() as db:
 		tz = db.get("timezone", 0)
 
-	t = get_NTP_time()
 	tz_minutes = int(abs(tz) % 100) * (1 if tz >= 0 else -1)
 	tz_hours = int(tz / 100)
 	t += (tz_hours * 3600) + (tz_minutes * 60)
